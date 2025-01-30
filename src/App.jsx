7872ebb0e0f0dd71d82f4b73401191b59eaaf258
M Habib Al Huda <m.habib.al.huda06@gmail.com>
2025-01-30 23:54:57 +0700

    feat: Add image upload input and style results section

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1>Image to Text</h1>
+        <h1>Converter</h1>
+
+        <div className="imageBox">
+          <input type="file" accept="image/*" />
+        </div>
+
+        <div className="results">
+          <div className="resultsBox">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
+              rerum, facilis perspiciatis nisi quas veniam, alias iusto unde
+              totam repellendus modi tenetur repellat, qui saepe quod omnis
+              laborum ea magni!
+            </p>
+          </div>
+          <p>copy</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
